eb931163f7b04d4b6f8c7d2a15c9a10a91edeb46
tjc0726 <49085201+tjc0726@users.noreply.github.com>
2020-04-12 11:36:23 +0800

    去除retrying功能 增加签到类型判断

--- chaoxingsign.py
+++ chaoxingsign.py
@@ -3,7 +3,9 @@ import json
 import time
 import datetime
 import numpy as np
-from retrying import retry
+import re
+import urllib3
+#from retrying import retry
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36'}#这个头目前没什么用,留着备用...
@@ -15,8 +17,10 @@ except FileNotFoundError:
     print('未找到配置文件')
     print('软件退出')
     exit()
-    
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+# def retry_if_connection_error(exception):
+#     return isinstance(exception, requests.exceptions.ConnectionError)
 class CxSign():
     def __init__(self,conf_user):  # 转换一下配置文件
         self.username = conf_user['username']
@@ -33,18 +37,17 @@ class CxSign():
             self.activates=np.load('activates.npy').tolist()
         except IOError:    #文件不存在
             self.activates=[]
-    def retry_if_connection_error(self,exception):
-        return isinstance(exception, requests.exceptions.ConnectionError)
-    @retry(retry_on_exception=retry_if_connection_error)
+#    @retry(retry_on_exception=retry_if_connection_error)
     def login(self):  # 获取cookie
         url = 'https://passport2-api.chaoxing.com/v11/loginregister'
         data = {'uname': self.username, 'code': self.passwd }
-        session = requests.session()
-        cookie_jar = session.post(url=url, data=data, headers=headers).cookies
+        self.session = requests.session()
+        cookie_jar = self.session.post(url=url, data=data, headers=headers).cookies
+        self.session.cookies=cookie_jar
         self.cookie = requests.utils.dict_from_cookiejar(cookie_jar)
         print('获取cookie成功')
         
-    @retry(retry_on_exception=retry_if_connection_error)
+#    @retry(retry_on_exception=retry_if_connection_error)
     def subject(self):  # 获取课程
         url = "http://mooc1-api.chaoxing.com/mycourse/backclazzdata"
         res = requests.get(url, headers=headers, cookies=self.cookie)
@@ -61,7 +64,7 @@ class CxSign():
             # classid.append(item['content']['id'])
         #print(self.item_all)
 
-    @retry(retry_on_exception=retry_if_connection_error)
+#    @retry(retry_on_exception=retry_if_connection_error)
     def taskactivelist(self):  # 查找签到任务
         url = "https://mobilelearn.chaoxing.com/ppt/activeAPI/taskactivelist"
         for i in self.item_all:
@@ -79,9 +82,9 @@ class CxSign():
                                     '[签到]', i['course']['data'][0]['name'], '查询到待签到活动 活动名称:%s 活动状态:%s 活动时间:%s aid:%s' % (
                                         item['nameOne'], item['nameTwo'], item['nameFour'], aid))
                                 self.aid_all.append(aid)    #所有进行中的签到任务
-                                self.sign(i['course']['data'][0]['name'],aid)  # 调用签到函数
+                                self.sign(item['nameOne'],i['course']['data'][0]['id'],i['id'],i['course']['data'][0]['name'],aid)  # 调用签到函数
 
-    @retry(retry_on_exception=retry_if_connection_error)
+#    @retry(retry_on_exception=retry_if_connection_error)
     def upload(self):  # 上传图片
         if self.picname == '':
             return
@@ -102,13 +105,57 @@ class CxSign():
             resdict = json.loads(uploadres.text)
             return (resdict['objectId'])
 
-    @retry(retry_on_exception=retry_if_connection_error)
-    def sign(self,course_name,aid):  # 签到,偷了个懒,所有的签到类型都用这个,我测试下来貌似都没问题
+#    @retry(retry_on_exception=retry_if_connection_error)
+    def sign(self,sign_type,course_id,class_id,course_name,aid):  # 签到,偷了个懒,所有的签到类型都用这个,我测试下来貌似都没问题
+        status=False    #一般签到就直接签完了 要加状态判断以用于决定后面的sever酱是否推送
         url = "https://mobilelearn.chaoxing.com/pptSign/stuSignajax"
-        objectId = self.upload()
-        data = {'name': self.name, 'address': self.address, 'activeId': aid, 'uid': self.cookie['UID'],'longitude': self.longitude, 'latitude': self.latitude, 'objectId': objectId}
-        res = requests.post(url, data=data, headers=headers, cookies=self.cookie)
-        print("签到状态:", res.text)
+        if '位置' in sign_type:
+            data = {
+                'name': self.name, 
+                'address': self.address,
+                'activeId': aid, 
+                'uid': self.cookie['UID'],
+                'longitude': self.longitude, 
+                'latitude': self.latitude, 
+                }
+        elif '手势' in sign_type:
+            data = {
+                'name': self.name, 
+                'activeId': aid, 
+                'uid': self.cookie['UID'],
+                'longitude': -1, 
+                'latitude': -1, 
+                }
+        elif '二维码' in sign_type:
+            data = {
+                'name': self.name, 
+                'activeId': aid, 
+                'uid': self.cookie['UID'],
+                'longitude': -1, 
+                'latitude': -1, 
+                }
+        else:
+            r = self.session.get('https://mobilelearn.chaoxing.com/widget/sign/pcStuSignController/preSign?activeId={}&classId={}&fid=39037&courseId={}'.format(aid,class_id,course_id),headers=headers,verify=False)
+            title = re.findall('<title>(.*)</title>', r.text)[0]
+            if "签到成功" not in title:
+                objectId = self.upload()
+                data = {
+                    'name': self.name, 
+                    'address': self.address, 
+                    'activeId': aid, 
+                    'uid': self.cookie['UID'],
+                    'longitude': self.longitude, 
+                    'latitude': self.latitude, 
+                    'objectId': objectId
+                    }
+            else:
+                status=True
+        if not status:
+            res = requests.post(url, data=data, headers=headers, cookies=self.cookie)
+            text=res.text
+        else:
+            text="success"
+        print("签到状态:", text)
         #这边不直接append的原因是长时间运行后这个列表肯定会越来越大
         #然后这边直接等于aid_all的话好像逻辑又有点奇怪 感觉好像activates是多余的
         self.activates=self.aid_all
@@ -118,11 +165,11 @@ class CxSign():
             np.save('activates.npy',np.array(self.aid_all))
         except OSError:
             pass
-        if (not self.SCKEY.isspace()) and res.text=='success':
+        if ((not self.SCKEY.isspace()) and (not self.SCKEY=='')) and text=='success':
             print('推送中')
-            self.push(course_name,res.text)
+            self.push(course_name,text)
 
-    @retry(retry_on_exception=retry_if_connection_error)
+#    @retry(retry_on_exception=retry_if_connection_error)
     def push(self,course_name,status):
         api = 'https://sc.ftqq.com/' + self.SCKEY + '.send'
         title = u"签到辣!"
